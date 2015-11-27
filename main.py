a9a138ff3c45384d55ecebe26c7f1c29dd43d63f
Roman Shkarin <mathii2009@gmail.com>
2015-11-27 17:27:26 +0100

    Add multicore fish converting.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import os
 import numpy as np
 from modules.tools.env import DataEnvironment
+from multiprocessing import Process
 from modules.tools.io import create_raw_stack, open_data, create_filename_with_shape, parse_filename
 from modules.tools.processing import binarizator
 from modules.tools.misc import Timer
@@ -20,13 +21,26 @@ import shutil
 base_save_path = "C:\\Users\\Administrator\\Documents\\afs_test_out\\%s"
 
 def convert_fish(fish_number):
+        print '----Converting fish #%d' % fish_number
         dir_path = "Z:\\grif\\ANKA_data\\2014\\XRegioMay2014\\tomography\\Phenotyping\\Recon\\fish%d\\Complete\\Corr" % fish_number
-
         out_path = "Z:\\grif\\Phenotype_medaka\\Originals\\%s"
         raw_data_stack = create_raw_stack(dir_path, "fish%d_proj_" % fish_number)
+        print '----Raw stack creating fish #%d' % fish_number
         raw_data_stack.tofile(out_path % ("fish%d_32bit_%dx%dx%d.raw" % \
             (fish_number, raw_data_stack.shape[2], raw_data_stack.shape[1], raw_data_stack.shape[0])))
         del raw_data_stack
+
+def convert_fish_in_parallel(fish_num_array):
+    t = Timer()
+
+    jobs = [Process(target=convert_fish, args=fn) for fn in fish_num_array]
+    for j in jobs:
+        j.start()
+        
+    for j in jobs:
+        j.join()
+
+    t.elapsed('Fish converting')
 
 def rotate_data_fish202():
     output_path = "C:\\Users\\Administrator\\Documents\\AFS_results\\fish%d"
